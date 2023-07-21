873bdab24e39102c9392bf90a342f37707e716af
Wedson Silva <wedsonsilva025@gmail.com>
2023-07-21 09:04:18 -0300

    create the etity seller

--- src/main/java/demojdbcdao/Program.java
+++ src/main/java/demojdbcdao/Program.java
@@ -1,9 +1,10 @@
 package demojdbcdao;
-
+import java.util.Date;
 public class Program {
 
     public static void main(String[] args) {
-        Department d = new Department("Books",1);
-        System.out.println(d);
+        Department department = new Department("Books",1);
+        Seller seller = new Seller(2,"wedson","wedson@gmail.com",new Date(),3000.0,department);
+        System.out.println(seller);
     }
 }
